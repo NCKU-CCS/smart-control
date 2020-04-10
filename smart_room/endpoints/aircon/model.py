b7ecd54adc65b39ae92670ba7b2f2385c2032697
ttw225 <P76084106@ncku.edu.tw>
2020-04-10 15:12:14 +0800

    feat(aircon): add two aircon actions

--- smart_room/endpoints/aircon/model.py
+++ smart_room/endpoints/aircon/model.py
@@ -6,7 +6,8 @@ from utils.base_models import SmartRoomBaseMixin
 
 class Aircon(db.Model, SmartRoomBaseMixin):
     __tablename__ = "aircon"
-    action = db.Column(db.String(80))
+    action_front = db.Column(db.String(80))
+    action_back = db.Column(db.String(80))
     time = db.Column(db.DateTime)
     user_id = db.Column(UUID(), db.ForeignKey("user.uuid"), nullable=False)
     user = db.relationship("User")
--- smart_room/endpoints/aircon/resource.py
+++ smart_room/endpoints/aircon/resource.py
@@ -1,20 +1,25 @@
 import os
-from datetime import datetime
 
 from flask import jsonify, make_response
 from flask_restful import Resource, reqparse
-import pytz
+from loguru import logger
 
-from utils.logging import logging
 from utils.oauth import auth, g
 
 from .model import Aircon
 
 
-def control_aircon(command):
-    cmd = f"irsend SEND_ONCE aircon {command}"
-    returned_value = os.system(cmd)
-    return returned_value
+def control_aircon(command_front, command_back):
+    returned_bool = True
+    for index, command in enumerate(command_front, command_back):
+        cmd = f"irsend SEND_ONCE aircon {command}"
+        returned = os.system(cmd)
+        if returned != 0:
+            returned_bool = False
+            logger.error(f"[control_aircon] {index} set to {command} Fail!")
+        else:
+            logger.success(f"[control_aircon] {index} set to {command} Success!")
+    return returned_bool
 
 
 class AirconResource(Resource):
@@ -24,7 +29,7 @@ class AirconResource(Resource):
     def _set_post_parser(self):
         self.post_parser = reqparse.RequestParser()
         self.post_parser.add_argument(
-            "action",
+            "action_front",
             type=lambda x: x
             if str(x[:-1]).isdigit()
             and 16 <= int(x[:-1]) <= 30
@@ -33,25 +38,37 @@ class AirconResource(Resource):
             else False,
             required=True,
             location="json",
-            help="Post Aircon : 'action' is required",
+            help="Post Aircon : 'action_front' is required",
+        )
+        self.post_parser.add_argument(
+            "action_back",
+            type=lambda x: x
+            if str(x[:-1]).isdigit()
+            and 16 <= int(x[:-1]) <= 30
+            and x[-1] == "c"
+            or x == "off"
+            else False,
+            required=True,
+            location="json",
+            help="Post Aircon : 'action_back' is required",
         )
 
     @auth.login_required
     def post(self):
-        logging.info(
+        logger.info(
             f"[Post Aircon Request]\nUser Account:{g.account}\nUUID:{g.uuid}\n"
         )
         args = self.post_parser.parse_args()
         if args["action"]:
             # Execute
-            returned_value = control_aircon(args["action"])
+            returned_bool = control_aircon(args["action_front"], args["action_back"])
             # Save to database
             record = {
-                "action": args["action"],
-                "time": datetime.now().astimezone(pytz.utc),
+                "action_front": args["action_front"],
+                "action_back": args["action_back"],
                 "user_id": g.uuid,
             }
             Aircon.add(Aircon(**record))
-            if returned_value == 0:
+            if returned_bool == 0:
                 return jsonify({"status": "success"})
         return make_response(jsonify({"status": "fail"}), 400)
